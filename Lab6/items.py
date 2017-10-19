e6e05273cf1309e7fba27497c83492595c806f5a
tomcompiles <>
2017-10-19 15:26:34 +0100

    My lab work

---
+++ Lab6/items.py
@@ -0,0 +1,72 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "mass" : 1,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "mass" : 1000,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "mass" : 100,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "mass" : 500,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "mass" : 50,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "mass" : 1500,
+
+    "description": "This student handbook explains everything. Seriously."
+}
+
+all_items = {
+    "id" : item_id,
+    "laptop" : item_laptop,
+    "money" : item_money,
+    "biscuits" : item_biscuits,
+    "pen" : item_pen,
+    "handbook" : item_handbook
+}
