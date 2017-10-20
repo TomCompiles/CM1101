4978b39067a4eec5b32e665d5e6619c2b8985d88
tomcompiles <>
2017-10-20 12:04:45 +0100

    fixed spacing

--- Lab6/game.py
+++ Lab6/game.py
@@ -201,7 +201,7 @@ def print_menu(exits, room_items, inv_items):
     # COMPLETE ME!
     #
     for item in room_items:
-        print("TAKE " + item["id"].upper() + " to take" + item["name"] + ".")
+        print("TAKE " + item["id"].upper() + " to take " + item["name"] + ".")
     for item in inv_items:
         print("DROP " + item["id"].upper() + " to drop your " + item["name"] + ".")
     
